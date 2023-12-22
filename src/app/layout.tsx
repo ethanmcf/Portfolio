514a19f01f71f612e6fd0928ff51207080f81dfe
ethanmcf <ethan_mcfarland@outlook.com>
2023-12-22 13:33:48 -0500

    Changed global font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,26 @@
 import type { Metadata } from "next";
 import "../../styles/globals.css";
+import { ReactElement } from "react";
+import SplitLayout from "../components/SplitLayout";
 
 export const metadata: Metadata = {
   title: "Ethan McFarland",
   description: "My portfolio website",
+  icons: {
+    icon: "icon.ico",
+  },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactElement }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <SplitLayout>
+      <div
+        className={
+          "flex-1 relative bg-backgroundStart text-text-dark inset-0 overflow-y-auto scroll-auto l-0"
+        }
+      >
+        {children}
+      </div>
+    </SplitLayout>
   );
 }
