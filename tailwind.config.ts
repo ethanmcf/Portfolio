ec15a95cdd2cbbf70d95cbb3af5aa58d23b2de2f
ethanmcf <ethan_mcfarland@outlook.com>
2023-12-22 13:11:51 -0500

    Updated config and folder structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,26 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      blue: "#7DD3FC",
+      white: "#FFFFFF",
+      black: "#000000",
+      text: {
+        dark: "#c9ced3",
+        light: "#000000",
       },
+      title: {
+        dark: "#FFFFFF",
+        light: "#59636f",
+      },
+      grey: "#5c6065",
+      darkGrey: "#0a111e",
+      background: {
+        dark: "#FFFFFF",
+        light: "#000000",
+      },
+      backgroundStart: "#030711",
+      backgroundEnd: "#051E44",
     },
   },
   plugins: [],
